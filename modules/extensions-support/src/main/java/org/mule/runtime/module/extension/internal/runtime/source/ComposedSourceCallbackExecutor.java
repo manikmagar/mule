f50e1d09b0cfca4c76c26e388743e74d49d6ff63
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-01-11 02:58:19 -0300

    weird watermark issue

---
+++ modules/extensions-support/src/main/java/org/mule/runtime/module/extension/internal/runtime/source/ComposedSourceCallbackExecutor.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.module.extension.internal.runtime.source;
+
+import static reactor.core.publisher.Mono.from;
+import org.mule.runtime.core.api.event.CoreEvent;
+import org.mule.runtime.extension.api.runtime.source.SourceCallbackContext;
+
+import java.util.Map;
+
+import org.reactivestreams.Publisher;
+
+public class ComposedSourceCallbackExecutor implements SourceCallbackExecutor {
+
+  private final SourceCallbackExecutor first;
+  private final SourceCallbackExecutor then;
+
+  public ComposedSourceCallbackExecutor(SourceCallbackExecutor first, SourceCallbackExecutor then) {
+    this.first = first;
+    this.then = then;
+  }
+
+  @Override
+  public Publisher<Void> execute(CoreEvent event, Map<String, Object> parameters, SourceCallbackContext context) {
+    return from(first.execute(event, parameters, context)).compose(v -> then.execute(event, parameters, context));
+  }
+}
